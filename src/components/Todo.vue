dd31656a895813c46ccd390660ee6215dd477ba4
yuanmeng <861013016@qq.com>
2020-10-30 08:54:35 +0800

    init

---
+++ src/components/Todo.vue
@@ -0,0 +1,16 @@
+<template>
+  <div>
+    <h1>Vite Miragejs Todo List Example</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Todo',
+  data() {
+    return {
+      count: 0,
+    }
+  },
+}
+</script>
